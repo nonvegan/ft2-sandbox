4a475d487cd3e99271e8a0d4cd80ee0a847d874e
nonvegan <pedromagalhaes_2000@hotmail.com>
2021-09-10 23:59:14 +0100

    Remove mkdir include files

--- main.c
+++ main.c
@@ -2,9 +2,6 @@
 #include <stdlib.h>
 #include <errno.h>
 #include <string.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <unistd.h>
 #include <freetype2/ft2build.h>
 #include FT_FREETYPE_H
 
